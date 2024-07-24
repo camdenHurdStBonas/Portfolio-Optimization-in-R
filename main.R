55a019b9c25e747e2f1d8cc15054bb4ebe71a65a
Camden Hurd <112513088+camdenHurdStBonas@users.noreply.github.com>
2024-07-24 13:06:52 -0400

    Create main.R
    added the main code

---
+++ main.R
@@ -0,0 +1,26 @@
+#   Set up
+
+#Cleared the work space
+rm(list=ls())
+#sets the variable "path" to the name of Working Directory
+path <- "/set/your/path/here"
+#Set the Working Directory using the variable "path"
+setwd(path)
+
+source("portfolio.R")
+
+source("common_start_date.R")
+
+source("ticker_names.R")
+
+stock_list <- get_common_etfs()
+
+common_start_date <- find_common_start_date(stock_list)
+
+#rf <- .05 / 242
+
+weights <- c(1, rep(0, length(stock_list) - 1))
+
+test <- portfolio(stock_list, from = common_start_date)
+
+test$summary(weights, A= 4)
